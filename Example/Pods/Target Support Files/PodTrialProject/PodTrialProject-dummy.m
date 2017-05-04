0675f2ea2a0ba1eff2e8051daaa0ac5b225feeac
Tejashri <Tejashri@Tejashris-Mac-mini.local>
2017-05-04 12:25:36 +0530

    trial commit for creating my own pod

---
+++ Example/Pods/Target Support Files/PodTrialProject/PodTrialProject-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_PodTrialProject : NSObject
+@end
+@implementation PodsDummy_PodTrialProject
+@end
---
+++ Example/Pods/Target Support Files/Pods-PodTrialProject_Tests/Pods-PodTrialProject_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PodTrialProject_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_PodTrialProject_Tests
+@end
